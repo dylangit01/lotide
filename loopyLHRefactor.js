d236a8ef70d70083ed1b1374699a6933570121d4
Dylan <dcren2010@gmail.com>
2021-03-29 23:04:40 -0400

    loopyLHRefactor.js updated

--- loopyLHRefactor.js
+++ loopyLHRefactor.js
@@ -1,19 +1,11 @@
 const loopyLighthouseDylan = (range, multiples, words) => {
   for (let i = range[0]; i <= range[1]; i++) {
     let output = '';
-    if (i % multiples[0] === 0) {
-      output += words[0];
-    }
-    if (i % multiples[1] === 0) {
-      output += words[1];
-    }
-    if (output === '') {
-      output = i;
-    }
+    i % multiples[0] === 0 ? output += words[0] : output;
+    i % multiples[1] === 0 ? output += words[1] : output;
+    output === '' ? output = i : output;
     console.log(output);
   }
 };
 
 loopyLighthouseDylan([15, 90], [2, 5], ['Batty', 'Beacon']);
-
-
